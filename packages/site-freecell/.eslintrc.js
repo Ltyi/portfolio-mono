2ca45165679cbc378e004dc6a78fbe8a6cccf5d2
Ryan <ltyi.box@gmail.com>
2022-03-30 00:11:31 +0800

    chore: vue init

---
+++ packages/site-freecell/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    '@vue/standard',
+    '@vue/eslint-config-typescript/recommended'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    'import/order': 'off',
+
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+
+    '@typescript-eslint/no-unused-vars': 'error'
+  }
+}
